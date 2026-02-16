8f01a4b07476bee3b3072702c09ae49f666b5cff
이휘 <dlgnl117@gmail.com>
2026-02-16 17:59:16 +0900

    260216 > Chapter01 > Playground 생성 및 실행 실습

---
+++ Chapter01/1.2_PlaygroundExample.playground/Contents.swift
@@ -0,0 +1,12 @@
+// 파일명: 1.2_PlaygroundExample.playground
+
+import UIKit // macOS용 기본 라이브러리입니다. (iOS라면 UIKit)
+
+// 1. 변수 선언
+var str = "Hello, playground"
+
+// 2. 값 수정
+str = "Hello, Swift World!"
+
+// 3. 출력
+print(str)

b619bb3fb57e5998d0f49f17cc7a3634c86047ab
이휘 <dlgnl117@gmail.com>
2026-02-16 18:31:46 +0900

    260216 > Chapter01 > if let 구문을 이용한 Optional Binding 실습

--- Chapter01/1.5_OptionalValue.swift.playground/Contents.swift
+++ Chapter01/1.5_OptionalValue.swift.playground/Contents.swift
@@ -9,3 +9,18 @@ var normalInt: Int = 10
 // 타입 뒤에 물음표(?)를 붙이면 "상자"에 담긴 상태가 됩니다.
 var optionalInt: Int? = 100
 optionalInt = nil // 이건 가능합니다! 값이 비어있을 수 있기 때문이죠.
+
+var optionalName: String? = "Hwi"
+
+// 1. 안전한 방법: Optional Binding (추천 ⭐)
+if let realName = optionalName {
+    // optionalName에 값이 있다면 realName이라는 임시 상수에 값이 담깁니다.
+    print("안녕하세요, \(realName)님!")
+} else {
+    // optionalName이 nil일 때 실행됩니다.
+    print("이름이 입력되지 않았습니다.")
+}
+
+// 2. 위험한 방법: Forced Unwrapping
+// 값이 확실히 있다고 보장될 떄만 써야 합니다.
+print(optionalName!)
